09c4aabe657e28a7c1e275d683f030ded0552252
Katie <antleredvixen@gmail.com>
2024-09-21 13:17:13 -0500

    pink gradient better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ffccff] to-[#ff00ff] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ff00ff] to-[#ffccff] text-white">
       [ vixen.sh in progress :3c ]
     </main>
   );
