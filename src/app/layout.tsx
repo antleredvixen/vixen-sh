df07c32b432af63adec8ece63f5c4af9ff1f3d71
Katie <antleredvixen@gmail.com>
2024-09-21 20:38:39 -0500

    minor topnav tweak

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,9 @@ export const metadata: Metadata = {
 function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>[ vixen.sh ]</div>
+      <div>vixen.sh</div>
 
-      <div>[ sign in ]</div>
+      <div>sign in</div>
     </nav>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ff00ff] to-[#ffccff] text-white">
-      [ vixen.sh in progress :3c ]
+      {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
     </main>
   );
 }
