e94ec9ef3fb3ef11be619bb177724202d5eb76b8
Katie <antleredvixen@gmail.com>
2024-09-21 22:01:44 -0500

    gallery test

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ vixen.sh is a personal blog and portfolio built with the T3 stack.
 
 - [X] Make it deploy (vercel)
 - [X] Scaffold basic ui with mock data
-- [ ] Tidy up build process
-- [ ] Actually set up a database (vercel postgres)
-- [ ] Attach database to UI
+- [X] Tidy up build process
+- [X] Actually set up a database (vercel postgres)
+- [X] Attach database to UI
 - [ ] Add authentication (w/ clerk)
 - [ ] Add image upload
 - [ ] Error management (w/ Sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,19 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const posts = await db.query.posts.findMany({
+    orderBy: (model, { desc }) => [desc(model.id)],
+  });
 
   console.log(posts);
 
   return (
-    <main className="flex min-h-[calc(100vh-60px)] flex-col items-center justify-center bg-gradient-to-b from-[#ff00ff] to-[#ffccff] text-white">
-      {posts.map((post) => (
-        <div key={post.id}>{post.name}</div>
+    <main className="flex flex-wrap gap-4 min-h-[calc(100vh-60px)] bg-gradient-to-b from-[#ff00ff] to-[#ffccff] text-white">
+      {posts.map((post, index) => (
+        <div key={post.id - index} className="flex w-48 flex-col">
+          <img src={post.url} />
+          <div>{post.name}</div>
+        </div>
       ))}
     </main>
   );
