6ec8b63a176b25d8db8840c63e825ee3cd78edb4
Katie <antleredvixen@gmail.com>
2024-09-22 23:48:09 -0500

    fixed page refactor syntax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,17 @@ async function Posts() {
     <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-4">
       {posts.map((post) => (
         <div key={post.id} className="mb-4 break-inside-avoid">
-        <img src={post.url} alt={`Post ${post.id}`} className="rounded-lg" />
-      </div>
-    ))}
-  </div>
+          <img src={post.url} alt={`Post ${post.id}`} className="rounded-lg" />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default async function HomePage() {
   return (
     <main className="p-4 min-h-[calc(100vh-100px)] bg-gradient-to-b from-[#ff00ff] to-[#ffccff] text-white">
+      <Posts />
     </main>
   );
 }
